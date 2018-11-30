6a4d260e4637b17274b8680d666e314d3a8a1357
Daniel Spandet Grønbjerg <dsg@commentor.dk>
2018-11-30 14:18:26 +0100

    Added user to message update

--- SignalR_example/wwwroot/js/notification.js
+++ SignalR_example/wwwroot/js/notification.js
@@ -2,12 +2,11 @@
 console.log("Loading notification.js");
 var connection = new signalR.HubConnectionBuilder().withUrl("/notificationHub").build();
 
-connection.on("ReceiveMessage", function(user, message){
-console.log(user + message);
-var li = document.createElement("li");
-li.textContent = message;
-document.getElementById("messagesList").appendChild(li);
-
+connection.on("ReceiveMessage", function (user, message) {
+    console.log(user + message);
+    var li = document.createElement("li");
+    li.textContent = user + " says: " + message;
+    document.getElementById("messagesList").appendChild(li);
 });
 
 connection.start().catch(function (err) {
@@ -17,7 +16,7 @@ connection.start().catch(function (err) {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    console.log("Sending: "+message);
+    console.log("Sending: " + message);
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
